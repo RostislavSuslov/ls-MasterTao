7f76e483b8baf01a926c7ae5c2948cdc70c81e16
RostSuslov <suslovros@gmail.com>
2023-03-04 12:11:30 +0200

    input file done

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -1,8 +1,32 @@
-// appendArrows: ".project-slider__nav",
 $(document).ready(function() {
     $('.banner').slick({
+        // dots: true,
+        arrows: true,
+        appendArrows: ".banner-nav",
+        responsive: [{
+                breakpoint: 991,
+                settings: {
+                    arrows: false,
+                    dots: true,
+                }
+            },
 
+        ]
     });
 
-    $('.your-class').slick();
+
 });
+
+
+
+const btnBox = document.querySelectorAll('.btn-box');
+console.log(btnBox);
+
+btnBox.forEach(item => {
+  console.log(item); 
+    item.closest('.col-md-6').classList.add('button-group');
+})
+
+document.getElementById('fileInput').onchange = function () {
+  document.getElementById('file-name').innerHTML = this.files[0].name;
+};
