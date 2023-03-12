317a62e468f34ebb4435a2a51e8617d61fcc1d95
RostSuslov <suslovros@gmail.com>
2023-03-12 23:10:50 +0200

    advantages

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -2,25 +2,30 @@ const SECTION = document.querySelectorAll('section');
 const CONTAINER_FLUID =  document.querySelectorAll('.container-fluid');
 console.log(CONTAINER_FLUID);
 
-CONTAINER_FLUID.forEach(element => {
-    const sectionHasFluid = element.closest('section');
 
+const containerMobile =()=> CONTAINER_FLUID.forEach(element => {
+    const sectionHasFluid = element.closest('section');
 
     if (sectionHasFluid.classList.contains('first-screen')){
         null
-    } else {
+    }
+    else {
         sectionHasFluid.closest('section').style.padding = '0 50px';
     }
-    
 })
 
 
-
-
-
-
-
-
+function adopContainer(x) {
+    if (x.matches) { // If media query matches
+        null
+    } else {
+        containerMobile();
+    }
+}
+  
+var x = window.matchMedia("(max-width: 991px)")
+adopContainer(x) // Call listener function at run time
+x.addListener(adopContainer) // Attach listener function on state changes
 
 
 
@@ -58,6 +63,35 @@ $(document).ready(function() {
     });
 
 
+    mobileOnlySlider("#slider-advantages", true, false,  991);
+
+    function mobileOnlySlider($slidername, $dots, $arrows, $breakpoint) {
+      var slider = $($slidername);
+      var settings = {
+        mobileFirst: true,
+        dots: $dots,
+        arrows: $arrows,
+        responsive: [
+          {
+            breakpoint: $breakpoint,
+            settings: "unslick"
+          }
+        ]
+      };
+    
+      slider.slick(settings);
+    
+      $(window).on("resize", function () {
+        if ($(window).width() > $breakpoint) {
+          return;
+        }
+        if (!slider.hasClass("slick-initialized")) {
+          return slider.slick(settings);
+        }
+      });
+    } // Mobile Only Slider
+    
+    
 });
 
 
